8f3a52e3cd991e7355a44e9207847f19d6925d8e
Tomas Dome <dometj@gmail.com>
2014-03-21 21:17:53 -0300

    Subo el proyecto inicial de players, luego hay que borrar los archivos que sean locales.

---
+++ player/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Player::Application.config.secret_key_base = 'ce6c59520be4b5ba1f1512d11b6e5c6dd78f2160decb0c81805526ff94f6dd0909a76a34154fcb88e051e61aa096f5495ceda8082964f58e444391d73eec8812'
