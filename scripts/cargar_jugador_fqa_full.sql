32725461568191a84a96b2d5a6df3578c092c4a5
ramoslm <ramoslautarom@gmail.com>
2014-06-27 18:05:54 -0300

    Cambie la vista de pivotes para agregarle el número de pivote. Cree el script de creación de la tabla jugador_fqa_full. Hice algunos cambios en el script que carga las firmas.

--- scripts/cargar_jugador_fqa_full.sql
+++ scripts/cargar_jugador_fqa_full.sql
@@ -1,48 +1,19 @@
-﻿CREATE OR REPLACE FUNCTION cargar_jugador_fqa_full() RETURNS integer AS $$
+﻿CREATE OR REPLACE FUNCTION cargar_jugador_fqa_full() RETURNS VOID AS $$
 DECLARE
-CANTIDAD_PIVOTES CONSTANT integer := 9;
-CANTIDAD_ATRIBUTOS CONSTANT integer := 55;
-contador_loop integer;
-firma real[] DEFAULT array_fill(-1, ARRAY[CANTIDAD_PIVOTES]);
--- selecciono los pivotes de prueba (9 pivotes elegidos)
-pivotes CURSOR FOR SELECT ARRAY[edad, aceleracion, agresividad, agilidad, anticipacion, balance,
-		  valentia, compostura, concentracion, corners, creatividad, centro,
-		  decision, determinacion, gambeta, remate, primer_toque, instinto,
-		  cabezazo, influencia, salto, tiro_lejano, pase_largo, marca,
-		  forma_fisica_natural, juego_sin_pelota, velocidad, pases, penales,
-		  posicionamiento, tiros_libres, resistencia, fuerza, entrada,
-		  trabajo_en_equipo, tecnica, sacrificio, porcentaje_atajadas,
-		  pierna_derecha, pierna_izquierda, arquero, libero, lateral_derecho,
-		  defensor_central, lateral_izquierdo,  carrilero_derecho, carrilero_izquierdo,
-		  mediocampista_defensivo, mediocampista_derecho, mediocampista_central,
-		  mediocampista_izquierdo, mediapunta_derecho, mediapunta_central,
-		  mediapunta_izquierdo, delantero] AS pivote_array
-	FROM jugador_norm WHERE id = ANY(ARRAY[5,9811,845,51174,111111,8777,216,656,1896]);
--- selecciono todos los jugadores normalizados 
-jugadores_norm CURSOR FOR SELECT ARRAY[edad, aceleracion, agresividad, agilidad, anticipacion, balance,
-		  valentia, compostura, concentracion, corners, creatividad, centro,
-		  decision, determinacion, gambeta, remate, primer_toque, instinto,
-		  cabezazo, influencia, salto, tiro_lejano, pase_largo, marca,
-		  forma_fisica_natural, juego_sin_pelota, velocidad, pases, penales,
-		  posicionamiento, tiros_libres, resistencia, fuerza, entrada,
-		  trabajo_en_equipo, tecnica, sacrificio, porcentaje_atajadas,
-		  pierna_derecha, pierna_izquierda, arquero, libero, lateral_derecho,
-		  defensor_central, lateral_izquierdo,  carrilero_derecho, carrilero_izquierdo,
-		  mediocampista_defensivo, mediocampista_derecho, mediocampista_central,
-		  mediocampista_izquierdo, mediapunta_derecho, mediapunta_central,
-		  mediapunta_izquierdo, delantero,jugador] AS jugador_array FROM jugador_norm;
+firma REAL[];
+-- selecciono los pivotes de prueba
+pivotes CURSOR FOR SELECT atributos_jugador AS pivote_array FROM pivotes ORDER BY numero_pivote;
+-- selecciono todos los jugadores normalizados estan
+jugadores_norm CURSOR FOR SELECT arrays_jugadores(ARRAY[j.id]) AS jugador_array FROM jugador_norm j;
 BEGIN	
 	-- para cada jugador
 	FOR jugador_actual IN jugadores_norm LOOP
-		-- inicializo un contador
-		contador_loop:=0;
 
 		-- creo la firma para este jugador:
 		-- para cada pivote calculo la distancia al jugador
 		FOR pivote IN pivotes LOOP	
-			contador_loop := contador_loop + 1;
 			-- firma[i] := distancia(jugador_actual,pivote):
-			firma[contador_loop] := distancia_jugadores_fqa_full(jugador_actual.jugador_array[1:55]::real[],pivote.pivote_array::real[]);
+			firma := array_append(firma, distancia_jugadores_fqa_full(jugador_actual.jugador_array::REAL[], pivote.pivote_array::REAL[]));
 		END LOOP;
 		
 		-- hago el insert con los datos de la firma
@@ -52,7 +23,7 @@ BEGIN
 		firma[5],firma[6],firma[7],firma[8],firma[9],firma[10],firma[11],firma[12],
 		firma[13],firma[14],firma[15],firma[16],firma[17],firma[18], jugador_actual.jugador_array[56]);
 	END LOOP;
-	RETURN 1;
+	RETURN;
 END
 $$ LANGUAGE "plpgsql";
 
@@ -65,4 +36,4 @@ SELECT cargar_jugador_fqa_full();
 -- borrar toda la tabla
 DELETE FROM jugador_fqa_full;
 
-COMMENT ON FUNCTION cargar_jugador_fqa_full() IS 'Calcula la firma fqa para cada elemento de la tabla jugador_norm, teniendo en cuenta todos los atributos de cada jugador.'
+COMMENT ON FUNCTION cargar_jugador_fqa_full() IS 'Calcula la firma fqa para cada elemento de la tabla jugador_norm, teniendo en cuenta todos los atributos de cada jugador.';
---
+++ scripts/tabla_jugador_fqa_full.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE jugador_fqa_full(
+	id SERIAL NOT NULL,
+	pivote1 REAL DEFAULT 0,
+	pivote2 REAL DEFAULT 0,
+	pivote3 REAL DEFAULT 0,
+	pivote4 REAL DEFAULT 0,
+	pivote5 REAL DEFAULT 0,
+	pivote6 REAL DEFAULT 0,
+	pivote7 REAL DEFAULT 0,
+	pivote8 REAL DEFAULT 0,
+	pivote9 REAL DEFAULT 0,
+	pivote10 REAL DEFAULT 0,
+	pivote11 REAL DEFAULT 0,
+	pivote12 REAL DEFAULT 0,
+	pivote13 REAL DEFAULT 0,
+	pivote14 REAL DEFAULT 0,
+	pivote15 REAL DEFAULT 0,
+	pivote16 REAL DEFAULT 0,
+	pivote17 REAL DEFAULT 0,
+	pivote18 REAL DEFAULT 0,
+	jugador INTEGER,
+CONSTRAINT pk_jugador_fqa_full PRIMARY KEY (id));
+
+ALTER TABLE jugador_fqa_full OWNER TO proyecto_gad;
+
+COMMENT ON TABLE jugador_fqa_full IS 'Esta tabla contiene las firmas de todos los jugadores.'
