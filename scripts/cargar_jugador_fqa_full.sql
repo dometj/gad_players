04e3d7ea940b0841e86f40bca22225896c0a6bdd
emiees <emilianoesalazar@gmail.com>
2014-07-10 20:19:02 -0300

    Arreglado el parametro en el script que carga la tabla de firmas

--- scripts/cargar_jugador_fqa_full.sql
+++ scripts/cargar_jugador_fqa_full.sql
@@ -14,7 +14,7 @@ BEGIN
 	-- inserto cada una de las firmas
 	FOR firma IN firmas LOOP
 		INSERT INTO jugador_fqa_full (pivote1, pivote2, pivote3, pivote4, pivote5, pivote6, pivote7, pivote8, pivote9, pivote10, pivote11, pivote12,
-		pivote13, pivote14, pivote15, pivote16, pivote17, pivote18, jugador) VALUES (firma.distancias[1], firma.distancias[2], firma.distancias[3],
+		pivote13, pivote14, pivote15, pivote16, pivote17, pivote18, jugador_norm) VALUES (firma.distancias[1], firma.distancias[2], firma.distancias[3],
 		firma.distancias[4], firma.distancias[5], firma.distancias[6], firma.distancias[7], firma.distancias[8], firma.distancias[9],
 		firma.distancias[10], firma.distancias[11], firma.distancias[12], firma.distancias[13], firma.distancias[14], firma.distancias[15],
 		firma.distancias[16], firma.distancias[17], firma.distancias[18], firma.id);
@@ -26,3 +26,8 @@ ALTER FUNCTION cargar_jugador_fqa_full()
   OWNER TO proyecto_gad;
 
 COMMENT ON FUNCTION cargar_jugador_fqa_full() IS 'Calcula la firma fqa para cada elemento de la tabla jugador_norm, teniendo en cuenta todos los atributos de cada jugador.';
+
+DROP FUNCTION cargar_jugador_fqa_full() 
+
+--Probar:
+SELECT cargar_jugador_fqa_full();
