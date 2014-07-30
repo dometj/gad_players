b043d8b2fb7ee88bea767956067126d72ba43a32
ramoslm <ramoslautarom@gmail.com>
2014-07-30 10:54:48 -0300

    Corregí la función normalizar. Agrego los scripts de las funciones para los pivotes generados.

---
+++ scripts/cargar_jugador_fqa_full_generados.sql
@@ -0,0 +1,23 @@
+﻿CREATE OR REPLACE FUNCTION cargar_jugador_fqa_full_generados() RETURNS VOID AS $BODY$
+BEGIN	
+	-- borro las firmas anteriores
+	DELETE FROM jugador_fqa_full_generados;
+	
+	-- inserto cada una de las firmas
+	INSERT INTO jugador_fqa_full_generados(firma, jugador)
+	SELECT array_agg(d.distancia), j.id
+	FROM (SELECT j.id, p.numero_pivote, distancia_jugadores_fqa_full(arrays_jugadores(ARRAY[j.id])::REAL[], p.pivote::REAL[]) AS distancia
+		FROM pivotes_generados p, jugador_norm j
+		ORDER BY j.id, p.numero_pivote) d
+	GROUP BY id;
+END
+$BODY$ LANGUAGE "plpgsql";
+
+ALTER FUNCTION cargar_jugador_fqa_full_generados()
+  OWNER TO proyecto_gad;
+
+COMMENT ON FUNCTION cargar_jugador_fqa_full() IS 'Calcula la firma fqa para cada elemento de la tabla jugador_norm, teniendo en cuenta los pivotes generados (ANDRES).';
+
+-- SELECT cargar_jugador_fqa_full_generados();
+
+-- SELECT * FROM jugador_fqa_full_generados LIMIT 10;
---
+++ scripts/n_vecinos_mas_cercanos_generados.sql
@@ -0,0 +1,28 @@
+﻿-- Algoritmo que devuelve los N jugadores mas cercanos (parecidos) a un jugador dado.
+-- Parámetros:
+	--jugador: elemento dado para realizar la busqueda a partir de el.
+	--N: cantidad de elementos parecidos que se necesitan encontrar.
+DROP FUNCTION n_vecinos_mas_cercanos_generados(jugador REAL[], n INTEGER);	
+CREATE OR REPLACE FUNCTION n_vecinos_mas_cercanos_generados(jugador REAL[], n INTEGER) RETURNS REAL[][] AS $BODY$
+DECLARE
+	jugadores REAL[][];
+	distancia REAL;
+	
+BEGIN
+	jugadores := '{}';
+	distancia := 50;
+	-- Mientras "jugadores" posea una cantidad menor a N de elementos, aumento en 1 la distancia y vuelvo a llamar a elemento_a_distancia_X con la nueva distancia.
+	LOOP
+		distancia := distancia + 2;
+		-- Llamo a elemento_a_distancia_X pasandole el jugador y la distancia inicial. Asigno el resultado a "jugadores"
+		jugadores := elemento_a_distancia_X_generados(jugador,distancia);
+		IF (array_length(jugadores, 1) > n) THEN
+			EXIT;
+		END IF;
+	END LOOP;
+
+	RETURN jugadores;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+ALTER FUNCTION n_vecinos_mas_cercanos_generados(REAL[], INTEGER) OWNER TO proyecto_gad;
